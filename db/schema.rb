ee7fa46476b22eb579b328d25595be9cc39714e4
Cristian Guerrero <cristian@s9.cl>
2016-05-23 15:37:22 -0700

    add auth raw data field to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523042453) do
+ActiveRecord::Schema.define(version: 20160523215828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160523042453) do
     t.datetime "updated_at",                          null: false
     t.string   "uid"
     t.string   "provider"
+    t.text     "auth_raw_data"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
