1ffd00a77d06f4b45334182aa7084c7768ad5a08
Cristian Guerrero <cristian@s9.cl>
2016-06-07 22:38:26 -0700

    Add button to remove dragged item from resumebox on new resume page, add js function to allow such ability and clean up the create_resume javascript file

--- app/assets/javascripts/resume_create.js
+++ app/assets/javascripts/resume_create.js
@@ -20,17 +20,18 @@ $(document).on('page:change', function(){
       });
     },
     over: function () {
-      //hides the placeholder when the item is over the sortable
+      //hides the placeholder (text on resumebox) when the item is over the sortable
       $(".placeholder").hide();
-      //removes the auto size added by sortable, adds class to style delete button
-      $('#resumebox .well-snippet').css({'width': 'auto', 'height': 'auto'}).addClass('resume-snippet');
+      //removes the auto size added by sortable
+      $('#resumebox .well-snippet').css({'width': 'auto', 'height': 'auto'});
       //removes the edit and delete buttons only from items in resumebox
       $("#resumebox .snippet-controls").remove();
+      //reveals a link which is used to remove snippet from resumebox
+      $('#resumebox .well-snippet .pull-right').removeClass('snippet-remove-hidden');
     }
   });
-  $('#trash').droppable({
-    drop: function(event, ui) {
-      ui.draggable.remove();
-    }
+  // this function listens on "resumebox" and binds to the "a href" to enable removing items from list.
+  $("#resumebox").on("click", "a", function(){
+    $(this).parent().parent().remove()
   });
 });
