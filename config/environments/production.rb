4c7e84f3c85c3b4db77d23abc21a2b100d9a4cc3
Cristian Guerrero <cristian@s9.cl>
2016-06-17 16:14:13 -0700

    google analytics via gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # replace this with your tracker code
+  GA.tracker = "UA-79547820-1"
 end
